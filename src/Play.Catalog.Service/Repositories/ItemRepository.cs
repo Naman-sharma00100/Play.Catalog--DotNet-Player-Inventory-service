99f1cfde7b71859ef841cc719ff4f15ae791c6f3
Naman-sharma00100 <naman.sharma00100@gmail.com>
2023-09-04 17:21:42 +0530

    implementing repository pattern with mongodb conn

---
+++ src/Play.Catalog.Service/Repositories/ItemRepository.cs
@@ -0,0 +1,56 @@
+using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
+
+namespace Play.Catalog.Service.Repositories
+{
+    public class ItemsRepository
+    {
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> dbcollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+
+        public ItemsRepository()
+        {
+            var mongoClient = new MongoClient("");
+            var database = mongoClient.GetDatabase("Catalog");
+            dbcollection = database.GetCollection<Item>(collectionName);
+
+        }
+
+        public async Task<IReadOnlyCollection<Item>> GetAllItemsAsync()
+        {
+            return await dbcollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+        public async Task<IReadOnlyCollection<Item>> GetItemAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterBuilder.Eq(entity => entity.Id, id);
+
+            return await dbcollection.Find(filter).ToListAsync();
+        }
+
+        public async Task CreateItemAsync(Item entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            await dbcollection.InsertOneAsync(entity);
+        }
+
+        public async Task UpdateItemAsync(Item entity)
+        {
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+            FilterDefinition<Item> filter = filterBuilder.Eq(existingEntity => existingEntity.Id, entity.Id);
+
+
+            await dbcollection.ReplaceOneAsync(filter, entity);
+        }
+
+        public async Task DeleteItemAsync(Guid id)
+        {
+            FilterDefinition<Item> filter = filterBuilder.Eq(entity => entity.Id, id);
+            await dbcollection.DeleteOneAsync(filter);
+        }
+
+
+    }
+}
